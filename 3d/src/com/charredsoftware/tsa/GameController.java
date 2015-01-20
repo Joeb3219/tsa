22206bc1fedfb3f7978855ef38c5e1e61d549a32
joeb3219 <joe@websitebegin.com>
2015-01-19 22:31:00 -0500

    Previous commits belong to 1.1.0 family

--- 3d/src/com/charredsoftware/tsa/GameController.java
+++ 3d/src/com/charredsoftware/tsa/GameController.java
@@ -34,7 +34,7 @@ public class GameController {
 
 	private static GameController _INSTANCE;
 	public String gameName = "The Enigma Machine";
-	public String version = "1.0.2";
+	public String version = "1.1.0";
 	public boolean developerMode = true, buildingMode = false, lighting = true, displayDialogs = true, showMainMenu = true, removeMobMode = false;
 	public boolean fullscreen = false;
 	public boolean applet = false;
